eb7a061a56593174761ae548e071e7435e8e9170
v <panagen3002@gmail.com>
2021-12-22 01:07:48 -0500

    more work on movement idk its broken still

--- game.js
+++ game.js
@@ -7,13 +7,28 @@ function grid() {
 
 function clicked(space) {
     pos = document.getElementById("pos").getAttribute("position");
-    spaceUp = space + 7;
+    console.log(`space: ${space}\npos: ${pos}`)
+    posUp = pos + 7;
+    posDown = pos - 7;
+
+    spaceUp = space + 7 
     spaceDown = space - 7;
+
     move = false;
+    open = false;
+
     if (pos == space + 1 || pos == space - 1 || pos == spaceUp || pos == spaceUp + 1 || pos == spaceUp - 1 || pos == spaceDown || pos == spaceDown - 1 || pos == spaceDown + 1){
+        move = true
+    }
+    if (pos % 7 == 0 && space - posDown == 1 || pos % 7 == 0 && space - pos == 1 || pos % 7 == 0 && space - posUp == 1) {
+        move = false
+    }
+    
+    if (move) {
         document.getElementById(pos).innerHTML = '';
         document.getElementById(space).innerHTML = `<img src="assets/player.png" alt ="a stick figure with a pointy hat">`;
         document.getElementById('pos').setAttribute("position", space);
+        
     }
 }
 
