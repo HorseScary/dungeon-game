233ed3fc76a7c4b9fc625df4a35cd3765195b4e9
v <panagen3002@gmail.com>
2021-12-21 22:39:59 -0500

    added a guy

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 function grid() {
-    for (let i = 0; i < 49; i++) {
-        document.getElementById("grid-container").innerHTML += `<div id="${i}" class="item"></div>`; 
+    for (let i = 1; i <= 49; i++) {
+        document.getElementById("grid-container").innerHTML += `<div id="${i}" class="item">${i}</div>`; 
     }
+    document.getElementById("28").innerHTML = '<img src="assets/player.png" alt="a stick figure with a pointy hat">';
 }
