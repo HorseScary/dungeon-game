36fc1d0128a120ae1fbb853098ca3d46fa8b1878
v <panagen3002@gmail.com>
2021-12-28 22:45:13 -0500

    trying to make the thing better

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 function grid() {
     // Creates the game board and places the player at the starting point
     for (let i = 1; i <= 49; i++) {
-        document.getElementById("grid-container").innerHTML += `<div id="${i}" class="item" occupied="false" onclick="clicked(${i})">${i}</div>`; 
+        document.getElementById("game-container").innerHTML += `<div id="${i}" class="item" occupied="false" onclick="clicked(${i})">${i}</div>`; 
     }
     document.getElementById("27").innerHTML = '<img src="assets/player.png" alt="a stick figure with a pointy hat">';
     document.getElementById("22").innerHTML = '<img src="assets/door.png" alt="a door">';
@@ -11,8 +11,10 @@ function grid() {
 
 }
 
-function clicked(space) {
+function isValid(space) {
+    // checks if a space is in range
     pos = document.getElementById("pos").getAttribute("position");
+    valid = false    
 
     posUp = pos + 7;
     posDown = pos - 7;
@@ -20,27 +22,32 @@ function clicked(space) {
     spaceUp = space + 7 
     spaceDown = space - 7;
 
-    move = false;
-    open = false;
-
-    // Checks if a space is valid
     if (pos == space + 1 || pos == space - 1 || pos == spaceUp || pos == spaceUp + 1 || pos == spaceUp - 1 || pos == spaceDown || pos == spaceDown - 1 || pos == spaceDown + 1){
-        move = true
+        valid = true
     }
     
     // Checks if the space is on the other side of the board
     if (pos % 7 == 0 && (space - 1) % 7 == 0) {
-        move = false;
+        valid = false;
     } 
     else if ((pos - 1) % 7 == 0 && space % 7 == 0) {
-        move = false;
+        valid = false;
     }
+    
+    return(valid)
+}
+
+function clicked(space) {
+    move = false;
+    open = false;
+
+    // Checks if a space is valid
     if (document.getElementById(space).getAttribute("occupied") != "false") {
         move = false
     }
 
     // Moves the player
-    if (move) {
+    if (isValid(space)) {
         document.getElementById(pos).innerHTML = '';
         document.getElementById(space).innerHTML = `<img src="assets/player.png" alt ="a stick figure with a pointy hat">`;
         document.getElementById('pos').setAttribute("position", space);
