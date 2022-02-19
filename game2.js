60d722f1ef9417c73ae17df290f43dfebb4d8f06
hors <horsescary@gmail.com>
2022-02-19 16:47:30 -0600

    game updates

--- game2.js
+++ game2.js
@@ -17,6 +17,17 @@ function placePlayer(playerPos) {
     playerTile.innerHTML = '<img src="assets/player.png" alt="a stick figure wearing a pointy hat">'
     
     document.getElementById(playerPos).appendChild(playerTile)
+    updatePlayerPos(playerPos)
+}
+function clearSpace(space) {
+    document.getElementById(space).innerHTML = ''
+}
+
+function getPlayerPosition() {
+    return(document.getElementById("player").getAttribute("pos"))
+}
+function updatePlayerPos(pos) {
+    document.getElementById("player").setAttribute("pos", toString(pos))
 }
 
 function isOccupied(space) {
@@ -30,14 +41,42 @@ function isOccupied(space) {
     }
 }
 
-function isSpaceValid(space) {
-    return(false)
+function isSpaceInRange(space) {
+    pos = getPlayerPosition()
+    valid = false
+
+    posUp = pos + 7;
+    posDown = pos - 7;
+
+    spaceUp = space + 7
+    spaceDown = space - 7;
+
+    if (pos == space + 1 || pos == space - 1 || pos == spaceUp || pos == spaceUp + 1 || pos == spaceUp - 1 || pos == spaceDown || pos == spaceDown - 1 || pos == spaceDown + 1) {
+        valid = true
+    }
+
+    // Makes sure the space is not on the other side of the board
+    if (pos % 7 == 0 && (space - 1) % 7 == 0) {
+        valid = false;
+    }
+    else if ((pos - 1) % 7 == 0 && space % 7 == 0) {
+        valid = false;
+    }
+
+    return (valid)
+}
+function movePlayer(space) {
+    clearSpace(getPlayerPosition())
+    placePlayer(space)
+    updatePlayerPos(space)
 }
 
 function clicked(space) {
-    if (!isOccupied(space)) {
-        if isSpaceValid(space) {
-            return(null)
+    itemInSpace = isOccupied(space)
+    if (!itemInSpace) {
+        if (isSpaceInRange(space)) {
+            console.log("space is in range!")
+            movePlayer(space)
         }
     }
     else {
